34ec08650553ac046d940980140ae6c6b3e29e8e
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-04-15 13:07:39 -0700

    add first bar chart

---
+++ main.js
@@ -0,0 +1,29 @@
+const dataset = [10, 20, 30, 40, 290, 50, 60, 70, 80]
+
+// bar styling and size
+const svgWidth = 500
+const svgHeight = 500
+const barPadding = 10
+const barWidth = (svgWidth/dataset.length)
+
+// set svg width & height on DOM
+const svg = d3.select('svg')
+.attr('width', svgWidth)
+.attr('height', svgHeight)
+
+// add data to svg
+const barChart = svg.selectAll('rect')
+.data(dataset)
+.enter()
+.append('rect')
+.attr('y', function(d) {
+    return svgHeight - d
+})
+.attr('height', function(d) {
+    return d
+})
+.attr('width', barWidth - barPadding)
+.attr('transform', function(d,i) {
+    const translate = [barWidth * i, 0]
+    return 'translate(' + translate + ')'
+})
